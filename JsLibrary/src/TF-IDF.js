b937502950e6902ebefdab1a241fb943f0979252
InDigitalLatvia <g.kaulinsh@inbox.lv>
2023-12-10 16:37:09 +0200

    Update TF-IDF.js

--- JsLibrary/src/TF-IDF.js
+++ JsLibrary/src/TF-IDF.js
@@ -1,4 +1,4 @@
-//=== TF, IDF, TF-IDF, BAG OF WORDS ============================================
+//=== TF, IDF, TF-IDF, BAG OF WORDS, VECTORS ===================================
 function computeTF(wordDict, bow) {
     let tfDict = {};
     let bowCount = bow.length;
@@ -37,4 +37,59 @@ function computeTFIDF(tfBow, idfs) {
         tfidf[word] = val * idfs[word];
     }
     return tfidf;
+}
+
+function docsToVectors(docs) {
+    const dict = {};
+    docs.forEach((doc, i) => {
+        doc.split(' ').forEach((word) => {
+            if (!dict[word]) {
+                dict[word] = Array(docs.length).fill(0);
+            }
+            dict[word][i]++;
+        });
+    });
+
+    // Aprēķinām TF un IDF
+    const tf = {};
+    const idf = {};
+    for (const word in dict) {
+        tf[word] = dict[word].map((count, i) => count / docs[i].split(' ').length);
+        idf[word] = Math.log(docs.length / dict[word].filter(Boolean).length);
+    }
+
+    // Aprēķinām TF-IDF
+    const tfidf = {};
+    for (const word in tf) {
+        tfidf[word] = tf[word].map((tfVal) => tfVal * idf[word]);
+    }
+
+    // Pārveidojam TF-IDF vektorus par dokumentu sarakstu
+    const vectors = docs.map((doc, i) => {
+        const vector = [];
+        for (const word in tfidf) {
+            vector.push(tfidf[word][i]);
+        }
+        return vector;
+    });
+
+    return {vectors, dict, tfidf};
+}
+
+function vectorsToDocs(vectors, dict, tfidf) {
+    // Pārveidojam vektorus par dokumentu sarakstu
+    const docs = vectors.map((vector, i) => {
+        const doc = [];
+        let index = 0;
+        for (const word in dict) {
+            const count = Math.round(vector[index] / tfidf[word][i]);
+            for (let j = 0; j < count; j++) {
+                doc.push(word);
+            }
+            index++;
+        }
+        return doc.join(' ');
+    });
+
+    return docs;
 }
